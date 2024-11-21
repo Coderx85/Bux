691cbc7218cda79f63f93a2b6d22a32f7197e534
Coderx85 <work.priyanshu085@gmail.com>
2024-11-21 22:53:54 +0530

    Add Header component with search and file uploader

---
+++ src/components/Search.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+
+import Image from "next/image";
+import { Input } from "@/components/ui/input";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+
+const Search = () => {
+  const [query, setQuery] = useState("");
+  const searchParams = useSearchParams();
+  const searchQuery = searchParams.get("query") || "";
+  const [open, setOpen] = useState(false);
+  const router = useRouter();
+  const path = usePathname();
+ 
+
+  return (
+    <div className="search">
+      <div className="search-input-wrapper">
+        <Image
+          src="/assets/icons/search.svg"
+          alt="Search"
+          width={24}
+          height={24}
+        />
+        <Input
+          value={query}
+          placeholder="Search..."
+          className="search-input"
+          onChange={(e) => setQuery(e.target.value)}
+        />
+
+        {open && (
+          <ul className="search-result">
+            {/* {results.length > 0 ? (
+              results.map((file) => (
+                <li
+                  className="flex items-center justify-between"
+                  key={file.$id}
+                  onClick={() => handleClickItem(file)}
+                >
+                  <div className="flex cursor-pointer items-center gap-4">
+                    <Thumbnail
+                      type={file.type}
+                      extension={file.extension}
+                      url={file.url}
+                      className="size-9 min-w-9"
+                    />
+                    <p className="subtitle-2 line-clamp-1 text-light-100">
+                      {file.name}
+                    </p>
+                  </div>
+
+                  <FormattedDateTime
+                    date={file.$createdAt}
+                    className="caption line-clamp-1 text-light-200"
+                  />
+                </li>
+              ))
+            ) : (
+              <p className="empty-result">No files found</p>
+            )} */}
+          </ul>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Search;
