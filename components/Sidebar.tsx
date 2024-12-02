7f404a218a22d8aeb038cbaf4de4ff9af66a654f
Coderx85 <work.priyanshu085@gmail.com>
2024-12-03 01:22:49 +0530

    Update sidebar styles and enhance navigation interactions

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -16,7 +16,7 @@ const Sidebar = ({ fullName, avatar, email }: Props) => {
   const pathname = usePathname();
 
   return (
-    <aside className="sidebar">
+    <aside className="sidebar bg-primary/30">
       <Link href="/">
         <Image
           src="/assets/icons/logo-full-brand.svg"
@@ -37,38 +37,43 @@ const Sidebar = ({ fullName, avatar, email }: Props) => {
 
       <nav className="sidebar-nav">
         <ul className="flex flex-1 flex-col gap-6">
-          {navItems.map(({ url, name, icon }) => (
-            <Link key={name} href={url} className="lg:w-full">
-              <li
-                className={cn(
-                  "sidebar-nav-item",
-                  pathname === url && "shad-active",
-                )}
-              >
-                <Image
-                  src={icon}
-                  alt={name}
-                  width={24}
-                  height={24}
+          {navItems.map(({ url, name, icon }) => {
+            const isActive = pathname === url || pathname.startsWith(`${url}/`);
+            return(
+              <Link key={name} href={url} className="group lg:w-full rounded-lg">
+                <li
                   className={cn(
-                    "nav-icon",
-                    pathname === url && "nav-icon-active",
+                    "sidebar-nav-item group-hover:shad-active ease-in duration-100 hover:animate-out",
+                    {
+                      "shad-active": isActive,
+                    }
                   )}
-                />
-                <p className="hidden lg:block">{name}</p>
-              </li>
-            </Link>
-          ))}
+                >
+                  <Image
+                    src={icon}
+                    alt={name}
+                    width={24}
+                    height={24}
+                    className={cn(
+                      "nav-icon group-hover:nav-icon-active group-hover:",
+                      pathname === url && "nav-icon-active scale-125",
+                    )}
+                  />
+                  <p className="hidden lg:block">{name}</p>
+                </li>
+              </Link>
+            )}
+          )}
         </ul>
       </nav>
 
-      <Image
+      {/* <Image
         src="/assets/images/files-2.png"
         alt="logo"
         width={506}
         height={418}
         className="w-full"
-      />
+      /> */}
 
       <div className="sidebar-user-info">
         <Image
